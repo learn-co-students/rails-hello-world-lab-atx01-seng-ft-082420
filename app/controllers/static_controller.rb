fc4d0c148894bbca5b51499618aab44544628e56
whychooseaname <68562400+whychooseaname@users.noreply.github.com>
2020-09-20 22:56:59 -0500

    Done.

---
+++ app/controllers/static_controller.rb
@@ -0,0 +1,7 @@
+class StaticController < ApplicationController
+    # Prevent CSRF attacks by raising an exception.
+    # For APIs, you may want to use :null_session instead.
+    def about
+        render "hello_world"
+    end
+  end
